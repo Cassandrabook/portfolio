44d04268bd81f9285db6575d2ee92b7dc396af3a
Your Namecl <you@example.com>
2023-03-30 15:00:41 +0200

    added dowload resume

--- src/components/Cv/Cv.tsx
+++ src/components/Cv/Cv.tsx
@@ -1,11 +1,13 @@
 import "./Cv.scss";
-import downloadcv from "portfolio/src/assets/Cassandra-book-cv.pdf";
+import cv from "./../../assets/Cassandra-book-cv.pdf";
 
 export const MyCv = () => {
   return (
     <section className="myCv">
       <h2 className="myCv__title">My resume</h2>
-      <a href={downloadcv}></a>
+      <a href={cv} download className="myCv__download">
+        Download resume
+      </a>
       <div className="myCv__png"></div>
     </section>
   );
