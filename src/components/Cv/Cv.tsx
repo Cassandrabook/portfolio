fe948ad2bf17fc2b514f830e49563600ced52a4f
Your Namecl <you@example.com>
2023-03-30 14:41:52 +0200

    added some email link and changed to english

--- src/components/Cv/Cv.tsx
+++ src/components/Cv/Cv.tsx
@@ -1,9 +1,11 @@
 import "./Cv.scss";
+import downloadcv from "portfolio/src/assets/Cassandra-book-cv.pdf";
 
 export const MyCv = () => {
   return (
     <section className="myCv">
       <h2 className="myCv__title">My resume</h2>
+      <a href={downloadcv}></a>
       <div className="myCv__png"></div>
     </section>
   );
