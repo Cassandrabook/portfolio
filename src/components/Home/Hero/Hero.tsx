d283adebf82116b6c8d4ca4ab4f779b63db8cabb
Your Namecl <you@example.com>
2023-04-03 14:20:21 +0200

    added some changes

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -8,7 +8,7 @@ export function Hero() {
       <section className="portfolio">
         <h1 className="portfolio__name slide-right">Cassandra Book</h1>
         <div className="portfolio__picture"></div>
-        <p className="portfolio__webb slide-right">Webb developer student</p>
+        <p className="portfolio__webb slide-right">Web developer student</p>
       </section>
       <About />
       <ShowProjects />
